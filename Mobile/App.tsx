e8661380aed4a94fd33244fa042adf473ba182f0
wesley_wisch <wesle_wisch@hotmail.com>
2022-09-12 14:39:11 -0300

    feat: Configuring the mobile environment

---
+++ Mobile/App.tsx
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button({ title }: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <Button title='Send 1' />
+      <Button title='Send 2' />
+      <Button title='Send 3' />
+      <Button title='Hello world' />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
