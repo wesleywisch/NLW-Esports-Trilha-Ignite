0f9bdd96c35578ef4d3c754a8dfafbc844793526
wesley_wisch <wesley_wisch@hotmail.com>
2022-09-15 09:46:40 -0300

    Feat: Separating some parts of the application into components, marking the component to display the game card, and marking the form to register a new ad.

---
+++ FrontEnd/src/components/GameBanner/index.tsx
@@ -0,0 +1,19 @@
+
+type GameBannerProps = {
+  bannerUrl: string;
+  title: string;
+  adsCount: number;
+}
+
+export function GameBanner({ adsCount, bannerUrl, title }: GameBannerProps) {
+  return (
+    <a href="" className='relative rounded-lg overflow-hidden'>
+      <img src={bannerUrl} alt={title} />
+
+      <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+        <strong className='font-bold text-white block'>{title}</strong>
+        <span className='text-zinc-300 text-sm block'>{adsCount} anúncio{`${adsCount > 1 ? 's' : ''}`}</span>
+      </div>
+    </a>
+  );
+}
